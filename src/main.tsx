cffd9818af003f889c6f46742f748efe391c3370
tywhisky <fty221@gmail.com>
2024-01-06 14:36:47 +0800

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <App />
   </React.StrictMode>,
 );
