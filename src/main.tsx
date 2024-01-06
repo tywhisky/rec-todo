6a44a517a9223ace4a0032d8a581919f0a96f15e
tywhisky <fty221@gmail.com>
2024-01-06 15:16:25 +0800

    Function

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { Theme } from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </React.StrictMode>,
 );
