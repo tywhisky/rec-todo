1e7e5a21c12883ba920e2b4dbf4110e3c9974e77
tywhisky <fty221@gmail.com>
2024-01-06 20:13:49 +0800

    Style

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="p-4 relative h-full">
       <List />
-      <div className="fixed bottom-6 w-11/12 left-8">
+      <div className="fixed bottom-6 w-8/12 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <TextField.Root >
           <TextField.Slot>
             <PlusIcon height="16" width="16" />
