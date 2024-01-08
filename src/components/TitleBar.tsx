ef95d467d598a389589dbcf4cfb78642d6c01cc0
tywhisky <fty221@gmail.com>
2024-01-08 20:57:13 +0800

    TitleBar Style

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,16 +1,11 @@
+import { Cross1Icon } from '@radix-ui/react-icons'
 import { appWindow } from '@tauri-apps/api/window'
 
 export default function TitleBar() {
   return (
-    <div data-tauri-drag-region className="titlebar bg-violet-400 border-violet-400">
-      <div onClick={() => appWindow.minimize()} className="titlebar-button" id="titlebar-minimize">
-        <div className="w-3 h-3 rounded-full bg-blue-500"></div>
-      </div>
-      <div onClick={() => appWindow.maximize()} className="titlebar-button" id="titlebar-maximize">
-        <div className="w-3 h-3 rounded-full bg-[#28c940]"></div>
-      </div>
+    <div data-tauri-drag-region className="titlebar bg-gradient-to-r from-[#d2aac2] to-[#a76cae] h-full">
       <div onClick={() => appWindow.close()} className="titlebar-button" id="titlebar-close">
-        <div className="w-3 h-3 rounded-full bg-[#ff5f58]"></div>
+        <Cross1Icon className="text-[#3b3247] mr-4" />
       </div>
     </div>
   )
