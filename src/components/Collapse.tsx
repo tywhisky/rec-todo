db9af3cc475d8cfc82002c2d32b11c641ba01fa2
tywhisky <fty221@gmail.com>
2024-01-15 20:43:07 +0800

    Change style for collpolse

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronRightIcon, ChevronDownIcon } from '@radix-ui/react-icons';
 import { useSpring, animated } from "@react-spring/web";
-import { Badge } from '@radix-ui/themes';
+import { Badge, Flex } from '@radix-ui/themes';
 
 export default function Collapse(props: any) {
   const [open, setOpen] = useState(false);
@@ -15,11 +15,13 @@ export default function Collapse(props: any) {
   return (
     <Collapsible.Root className="w-full" open={open} onOpenChange={setOpen}>
       <Collapsible.Trigger asChild>
-        <div className="w-[150px] cursor-pointer select-none hover:bg-teal4 bg-teal3 px-2 rounded-lg py-1 flex items-center justify-between mb-2">
-          {open ? <ChevronDownIcon className='teal12' /> : <ChevronRightIcon className='teal12' />}
-          <span className="text-teal12 font-light text-[15px] leading-[25px]">
-            Completed
-          </span>
+        <div className="w-[150px] transition duration-300 ease-in-out cursor-pointer select-none hover:bg-teal4 bg-teal3 px-2 rounded-lg py-[2px] flex items-center justify-between my-2">
+          <Flex align="center">
+            {open ? <ChevronDownIcon className='teal12' /> : <ChevronRightIcon className='teal12' />}
+            <span className="text-teal12 font-light text-[14px] leading-[25px] ml-1">
+              Completed
+            </span>
+          </Flex>
           <Badge className='text-teal11'>{props.completedQty || 0}</Badge>
         </div>
       </Collapsible.Trigger>
